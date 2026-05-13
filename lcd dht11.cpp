1f5358108f9e8f173ae460a7bdf592246a10bdbd
arleyont <120304803+ArleyKun@users.noreply.github.com>
2026-05-13 08:45:06 +0800

    Create lcd dht11.cpp

---
+++ lcd dht11.cpp
@@ -0,0 +1,74 @@
+#include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+
+SoftwareSerial fromESP(10, 11);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte degreeChar[8] = {
+  0b00110, 0b01001, 0b01001, 0b00110,
+  0b00000, 0b00000, 0b00000, 0b00000
+};
+
+void setup() {
+  Serial.begin(9600);
+  fromESP.begin(9600);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.createChar(0, degreeChar);
+
+  lcd.setCursor(0, 0);
+  lcd.print("  Room 508 Mon  ");
+  lcd.setCursor(0, 1);
+  lcd.print(" Waiting data...");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+  if (fromESP.available()) {
+    String data = fromESP.readStringUntil('\n');
+    data.trim();
+
+    Serial.println("Received: " + data);
+
+    if (data == "ERR") {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Sensor Error!   ");
+      lcd.setCursor(0, 1);
+      lcd.print("Check DHT11     ");
+      return;
+    }
+
+    float temp = -1, hum = -1, hindex = -1;
+    int tIdx  = data.indexOf("T:");
+    int hIdx  = data.indexOf(",H:");
+    int hiIdx = data.indexOf(",HI:");
+
+    if (tIdx != -1 && hIdx != -1 && hiIdx != -1) {
+      temp   = data.substring(tIdx + 2, hIdx).toFloat();
+      hum    = data.substring(hIdx + 3, hiIdx).toFloat();
+      hindex = data.substring(hiIdx + 4).toFloat();
+    }
+
+    lcd.setCursor(0, 0);
+    lcd.print("T:");
+    lcd.print(temp, 1);
+    lcd.write(byte(0));
+    lcd.print("C ");
+    lcd.print("H:");
+    lcd.print((int)hum);
+    lcd.print("%  ");
+
+    lcd.setCursor(0, 1);
+    lcd.print("HI:");
+    lcd.print(hindex, 1);
+    lcd.write(byte(0));
+    lcd.print("C ");
+    if (hindex < 27)       lcd.print("Comfy  ");
+    else if (hindex < 32)  lcd.print("Warm   ");
+    else if (hindex < 41)  lcd.print("Caution");
+    else                   lcd.print("DANGER!");
+  }
+}
