e9b1f29f0c84e1e82d18acdb0b65aa27fae63613
arleyont <120304803+ArleyKun@users.noreply.github.com>
2026-05-17 13:34:19 +0800

    Create audio visual.cpp

---
+++ audio visual.cpp
@@ -0,0 +1,57 @@
+/* 
+AROMIN, Arley Bentley S.
+BAET 1 - A
+This code was modified in collaboration with  with De Leon, Haro, Mata, and Sagadraca
+*/
+
+int soundPin = A0;
+int LED1 = 2;
+int LED2 = 3;
+int LED3 = 4;
+int LED4 = 5;
+int LED5 = 6;
+int LED6 = 7;
+int LED7 = 8;
+int LED8 = 9;
+int LED9 = 10;
+int LED10 = 11;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
+  pinMode(LED8, OUTPUT);
+  pinMode(LED9, OUTPUT);
+  pinMode(LED10, OUTPUT);
+}
+
+void loop()
+{
+  long sum = 0;
+
+  for(int i = 0; i < 100; i++)
+  {
+    sum += analogRead(soundPin);
+  }
+
+  sum = sum / 100;
+
+  if (sum >= 100) digitalWrite(LED1, HIGH); else digitalWrite(LED1, LOW);
+  if (sum >= 150) digitalWrite(LED2, HIGH); else digitalWrite(LED2, LOW);
+  if (sum >= 200) digitalWrite(LED3, HIGH); else digitalWrite(LED3, LOW);
+  if (sum >= 225) digitalWrite(LED4, HIGH); else digitalWrite(LED4, LOW);
+  if (sum >= 250) digitalWrite(LED5, HIGH); else digitalWrite(LED5, LOW);
+  if (sum >= 275) digitalWrite(LED6, HIGH); else digitalWrite(LED6, LOW);
+  if (sum >= 300) digitalWrite(LED7, HIGH); else digitalWrite(LED7, LOW);
+  if (sum >= 325) digitalWrite(LED8, HIGH); else digitalWrite(LED8, LOW);
+  if (sum >= 350) digitalWrite(LED9, HIGH); else digitalWrite(LED9, LOW);
+  if (sum >= 400) digitalWrite(LED10, HIGH); else digitalWrite(LED10, LOW);
+
+  Serial.println(sum);
+}
